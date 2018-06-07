b42ca78ea191f11e4182f2c3c2b957a8006294c1
BoHau Huang <31922162+BoHauHuang@users.noreply.github.com>
2018-06-08 02:56:07 +0800

    Add files via upload

--- MPU6050_test.m
+++ MPU6050_test.m
@@ -9,7 +9,7 @@ j = 1;
 trigger = -1
 fopen(scom);
 fprintf(scom,'a')
-c1 = clock
+c1 = clock;
 while j <= 5000
     if (scom.BytesAvailable)
         t = fgetl(scom);
@@ -18,12 +18,12 @@ while j <= 5000
         gy(j) = str2num(t{3});
         gz(j) = str2num(t{4});
         if j > 1 &&abs(gz(j)-gz(j-1)) > 1000 && trigger < 0
-            trigger = j
+            trigger = j;
         end
         j = j+1;
     end
 end
-c2 = clock
+c2 = clock;
 fclose(scom)
 j./(c2(5)*60+c2(6)-c1(5)*60-c1(6))
 figure();
@@ -36,23 +36,23 @@ hold off;
 gx = gx(trigger-10:trigger+500);
 gy = gy(trigger-10:trigger+500);
 gz = gz(trigger-10:trigger+500);
-figure();
-plot(gx);
-hold on;
-plot(gy);
-plot(gz);
-hold off;
+% figure();
+% plot(gx);
+% hold on;
+% plot(gy);
+% plot(gz);
+% hold off;
 
 gxf = fft(gx);
 gyf = fft(gy);
 gzf = fft(gz);
 
-figure();
-hold on;
-plot(gxf(2:end));
-plot(gyf(2:end));
-plot(gzf(2:end));
-hold off;
+% figure();
+% hold on;
+% plot(gxf(2:end));
+% plot(gyf(2:end));
+% plot(gzf(2:end));
+% hold off;
 
 gxp = phase(gxf);
 gyp = phase(gyf);
@@ -61,15 +61,17 @@ gxf = abs(gxf);
 gyf = abs(gyf);
 gzf = abs(gzf);
 
-figure();
-plot(gxf(2:end));
-hold on;
-plot(gyf(2:end));
-plot(gzf(2:end));
-hold off;
-figure();
-hold on;
-plot(gxp(2:end));
-plot(gyp(2:end));
-plot(gzp(2:end));
-hold off;
+% figure();
+% plot(gxf(2:end));
+% hold on;
+% plot(gyf(2:end));
+% plot(gzf(2:end));
+% hold off;
+% figure();
+% hold on;
+% plot(gxp(2:end));
+% plot(gyp(2:end));
+% plot(gzp(2:end));
+% hold off;
+signal = [gxp(2:end)',gxf(2:end)',gyp(2:end)',gyf(2:end)',gzp(2:end)',gzf(2:end)'];
+csvwrite('7/7_'+int2str(count), signal);
